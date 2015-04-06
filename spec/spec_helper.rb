94fce19e55051ea53b7e486659548ea35324d3d4
Vangelis Tsoumenis <kioopi@gmail.com>
2015-04-06 18:25:05 +0200

    Adds factory girl config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,3 +17,14 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
 end
+
+RSpec.configure do |config|
+  # Allow to use build and create methods without FactoryGirl prefix.
+  config.include FactoryGirl::Syntax::Methods
+
+  # Make sure factories are up to date when using spring
+  config.before(:all) do
+    FactoryGirl.reload
+  end
+end
+
